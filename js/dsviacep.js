345e5a9d77308da9ee9db0b9f6f04461099ae005
Paulo Sergio <paulosergioilha38@gmail.com>
2022-12-13 13:51:03 -0300

    first commit

---
+++ js/dsviacep.js
@@ -0,0 +1,11 @@
+import * as fromController from './controllers/form-controller.js';
+import * as listController from './controllers/list-controller.js';
+import * as modalController from './controllers/modal-controller.js';
+import * as pageController from './controllers/page-controller.js';
+
+
+
+fromController.init();
+listController.init();
+modalController.init();
+pageController.init();
